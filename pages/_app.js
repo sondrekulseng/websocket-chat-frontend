5d009692af5172a7da1f2400ccad450d27d4943f
Sondre Kulseng <>
2023-10-28 18:15:51 +0200

    Update viewport

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,7 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
       </Head>
       <Component {...pageProps} />
     </>
