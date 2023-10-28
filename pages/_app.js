8c42e9a0bcf72bcde8634e04d89c1d508e941cba
Sondre Kulseng <>
2023-10-29 01:00:54 +0200

    Update

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,7 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
+          <title>Websocket chat</title>
           <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
       </Head>
       <Component {...pageProps} />
--- pages/index.js
+++ pages/index.js
@@ -5,9 +5,11 @@ import { Header } from '../components/Header';
 export default function HomePage() {
 
   return (
-    <div>
+    <div style={{paddingTop: "5vh", width: "80%", margin: "auto"}}>
       <h3>Websocket chat</h3>
       <FormLogin/>
+      <br/>
+    <p>Application created by <a href="https://sondre.kulseng.no">Sondre Kulseng</a></p>
     </div>
   );
 }
